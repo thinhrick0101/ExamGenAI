f3e7b3c5deaa2be5ca96698286e4af843c6811a5
thinhrick0101 <thinhrickgiadinh@gmail.com>
2025-02-04 04:22:59 +0700

    update html

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "Generative AI Project",
-    version= '0.0.0',
+    name = "Exam Generator",
+    version= '0.0.1',
     author="Nguyen Duc Thinh",
-    author_email="",
+    author_email="nguyen.djuc.thinh@student.vu.nl",
     packages= find_packages(),
     install_requires = []
 )
